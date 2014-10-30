61820e66a8f6eaebf41ba0125d06e39bec4c8c59
Vladimir Dobriakov <vladimir@geekq.net>
2014-10-30 18:02:56 +0100

    Fix 'Oops! Cannot load necessary modules: No module named TiffImagePlugin'
    s. also http://code.google.com/p/diffraction-ring-profiler/issues/detail?id=2
    https://bugs.launchpad.net/ubuntu/+source/impressive/+bug/1166680

--- impressive.py
+++ impressive.py
@@ -182,7 +182,7 @@ try:
     import pygame
     from pygame.locals import *
     import Image, ImageDraw, ImageFont, ImageFilter
-    import TiffImagePlugin, BmpImagePlugin, JpegImagePlugin, PngImagePlugin, PpmImagePlugin
+    from PIL import TiffImagePlugin, BmpImagePlugin, JpegImagePlugin, PngImagePlugin, PpmImagePlugin
 except (ValueError, ImportError), err:
     print >>sys.stderr, "Oops! Cannot load necessary modules:", err
     print >>sys.stderr, """To use Impressive, you need to install the following Python modules:
